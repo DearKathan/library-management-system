c71c7e0c242f9a66b170f5f1bd6f8515f41bc214
Kathan <kathanshah2006@gmail.com>
2024-11-04 20:40:40 +0530

    Printing books

--- crud.py
+++ crud.py
@@ -1,8 +1,18 @@
 from models import Book
+# ,Member
 from database import session
 
 def add_book(title, author, isbn, count):
-    print("Adding book to database")
-    book = Book(title=title, author= author, isbn=isbn, count= count)
+    print("Adding book to the database..")
+    book = Book(title = title, author = author, isbn = isbn, count = count)
     session.add(book)
     session.commit()
+
+def get_book():
+    return session.query(Book).all()
+
+# def add_member(name, email):
+#     print("Processing..")
+#     member = Member(name = name, email = email)
+#     session.add(member)
+#     session.commit()
--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from crud import add_book
+from crud import add_book, get_book
+# ,add_member
+
+def printBooks():
+    books = get_book()
+    for book in books:
+        available = "Available" if book.count > 0 else "Not available"
+        print(f"{book.id}: '{book.title}' by {book.author} (ISBN: f{book.isbn}) -  {available} ({book.count} copies)")
+
+
+def addNewBook():
+    title = input("Enter title of the book: ")
+    author = input("Enter author name of the book: ")
+    isbn = input("Enter ISBN number of the book: ")
+    count = int(input("Enter total number of the books required: "))
+    add_book(title, author, isbn, count)
 
 def main():
     print("************************")
@@ -8,16 +23,17 @@ def main():
     choice = input("Enter your choice: ")
 
     if choice == '1':
-        title = input("Enter title of the book: ")
-        author = input("Enter author name of the book: ")
-        isbn = input("Enter ISBN number of the book: ")
-        count = int(input("Enter total number of the books required: "))
-        add_book(title, author, isbn, count)
+        addNewBook()
+    
+    # elif choice == '2':
+    #     name = input("Enter title of the book: ")
+    #     email = input("Enter author name of the book: ")
+    #     add_member(name, email)
     
     elif choice == '2':
-        print("Code to view book")
+        printBooks()
     else:
-        print("WRONG CHOICE ENTERED!!")
+        print("Wrong choice entered!!")
 
 
 if __name__ == "__main__":
